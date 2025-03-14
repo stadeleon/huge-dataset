596ba2c6b0198daa838299f13caff3893ad603fb
Leonid Derevianko <leo.mrakobes@gmail.com>
2025-03-14 03:02:44 +0100

    initial commit

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ -z "$(ls -A /var/www/app)" ]; then
+    echo "Dir /var/www/app is empty. Downloading Symfony skeleton..."
+    composer create-project symfony/skeleton:"^6.4" /var/www/app
+    composer require symfony/webapp-pack
+    composer require ramsey/uuid-doctrine
+    composer require nelmio/api-doc-bundle
+
+#    php bin/console doctrine:migrations:migrate
+#    php bin/console doctrine:migrations:migrate --env=test
+
+fi
+
+exec php-fpm
